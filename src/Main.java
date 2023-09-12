045cd0b6c4eaffe934e1c88c0e79f1a075132f83
tonyshu <tonynshu@gmail.com>
2023-09-12 14:01:21 +0800

    i finished the ide test woohoo!!!!

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(1, 1);
+        System.out.println(resultOne); //should print 2
+        String resultTwo = greeting("tony");
+        System.out.println(resultTwo); //should print "Bonjour, tony!
+        int resultThree = add(1, 1, 1, 1);
+        System.out.println(resultThree); //should print 4
+        printMe("this was a triumph, i'm making a note here huge success!");
+    }
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String greeting (String name) {
+        return "Bonjour, " + name + "!";
+    }
+    public static int add (int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(numOne, numTwo), add(numThree, numFour));
+    }
+    public static void printMe(String toBePrinted) {
+        System.out.println(toBePrinted);
+    }
 }
